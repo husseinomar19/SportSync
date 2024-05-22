88e51613e25e6d4633a47c0f300a33a579ff7b5f
hussein <118814567+husseinomar19@users.noreply.github.com>
2024-05-22 14:23:26 +0200

    foto

--- sportsync/src/app/page.tsx
+++ sportsync/src/app/page.tsx
@@ -1,7 +1,23 @@
 import Image from "next/image";
 import Header from "./componeten/Header";
+import "./globals.css";
 export default function Home() {
   return (
+    <>
     <Header />
+    <div className="home_hero">
+        <div className="home_info">
+          <img src="/logonaam.png" alt="logo naam" />
+          <h2>
+          ONTDEK, VERBIND EN GROEI SAMEN MET ONZE LEVENDIGE SPORTGEMEENSCHAP.
+          </h2>
+          <p>Word lid van onze bruisende sportgemeenschap en ontdek nieuwe verbindingen en kansen. Samen inspireren we elkaar om grenzen te verleggen en nieuwe hoogten te bereiken voor een sterker en gezonder leven.</p>
+          <div className="ontdekmeer">
+            <p>ontdek meer</p>
+            <img src="/pijl.png" alt="ontdekmeer" />
+          </div>
+        </div>
+    </div>
+    </>
   );
 }
