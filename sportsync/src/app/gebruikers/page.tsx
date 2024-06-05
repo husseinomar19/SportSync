168e1c8d1a3f774be54bba3264f973d498eda367
hussein <118814567+husseinomar19@users.noreply.github.com>
2024-06-05 14:29:09 +0200

    user lyout gemaakt

---
+++ sportsync/src/app/gebruikers/page.tsx
@@ -0,0 +1,158 @@
+'use client';
+export default function Gebruiker(){
+    return(
+        <>
+        <header id="home_hero">
+            <div className="head">
+                 <img src="/usericon.png" alt="zoeken" />
+                <div className="samen">
+                <p>Samen sporten, samen groeien!</p>
+                <span className="span_een"></span>
+                <span className="span_twee"></span>
+                </div>
+              <div className="header_user">
+                  <a href=""><img src="/chat.png" alt="" /></a>
+                  <a href=""><img src="/posten_img.png" alt="" /></a>
+
+                   <div className="user_titel">
+                    <h4>Hussein Omar</h4>
+                    <img src="usericon.png" alt="User" />
+                   </div>
+              </div>
+            </div>
+
+            <div className="logo_gebruiker">
+                <a href="/"><img src="/logo.png" alt="logo" /></a>
+               </div>     
+        </header>
+
+        <div className="Posten">
+
+            <div className="posten_items">
+
+                <div className="user_naam">
+                    <img src="usericon.png" alt="user" />
+                    <div className="user_naam_titel">
+                        <h4>Hussein Omar</h4>
+                        <p>20 m</p>
+                    </div>
+                </div>
+
+                <div className="post_teskt">
+                    <p>💪 Ontketen je potentieel in de sportschool! 💥 Word sterker, fitter en gezonder. Doe mee! #GymLife 🏋️‍♂️</p>
+                </div>
+        
+               <div className="post_img">
+                  <img src="homeimg.png" alt="user" />
+               </div>
+
+               <div className="post_like">
+                  <p>100 Like</p>
+                  <p>20 opmerkingen</p>
+               </div>
+
+
+               <div className="post_likes">
+                <div className="like">
+                    <img src="/like.png" alt="" />
+                    <p>Vind ik leuk</p>
+                </div>
+                <div className="like">
+                    <img src="/opmerking.png" alt="" />
+                    <p>opmerkingen</p>
+                </div>
+                <div className="like">
+                    <img src="/delen.png" alt="" />
+                    <p>delen</p>
+                </div>
+               </div>
+
+            </div>
+
+            <div className="posten_items">
+
+<div className="user_naam">
+    <img src="usericon.png" alt="user" />
+    <div className="user_naam_titel">
+        <h4>Hussein Omar</h4>
+        <p>20 m</p>
+    </div>
+</div>
+
+<div className="post_teskt">
+    <p>💪 Ontketen je potentieel in de sportschool! 💥 Word sterker, fitter en gezonder. Doe mee! #GymLife 🏋️‍♂️</p>
+</div>
+
+<div className="post_img">
+  <img src="homeimg.png" alt="user" />
+</div>
+
+<div className="post_like">
+  <p>100 Like</p>
+  <p>20 opmerkingen</p>
+</div>
+
+
+<div className="post_likes">
+<div className="like">
+    <img src="/like.png" alt="" />
+    <p>Vind ik leuk</p>
+</div>
+<div className="like">
+    <img src="/opmerking.png" alt="" />
+    <p>opmerkingen</p>
+</div>
+<div className="like">
+    <img src="/delen.png" alt="" />
+    <p>delen</p>
+</div>
+</div>
+
+</div>
+<div className="posten_items">
+
+<div className="user_naam">
+    <img src="usericon.png" alt="user" />
+    <div className="user_naam_titel">
+        <h4>Hussein Omar</h4>
+        <p>20 m</p>
+    </div>
+</div>
+
+<div className="post_teskt">
+    <p>💪 Ontketen je potentieel in de sportschool! 💥 Word sterker, fitter en gezonder. Doe mee! #GymLife 🏋️‍♂️</p>
+</div>
+
+<div className="post_img">
+  <img src="homeimg.png" alt="user" />
+</div>
+
+<div className="post_like">
+  <p>100 Like</p>
+  <p>20 opmerkingen</p>
+</div>
+
+
+<div className="post_likes">
+<div className="like">
+    <img src="/like.png" alt="" />
+    <p>Vind ik leuk</p>
+</div>
+<div className="like">
+    <img src="/opmerking.png" alt="" />
+    <p>opmerkingen</p>
+</div>
+<div className="like">
+    <img src="/delen.png" alt="" />
+    <p>delen</p>
+</div>
+</div>
+
+</div>
+
+
+
+        </div>
+        </>
+    )
+}
--- sportsync/src/app/inlog/page.tsx
+++ sportsync/src/app/inlog/page.tsx
@@ -1,15 +1,40 @@
+'use client';
 import Image from "next/image";
 import  Header from "../componeten/Header";  
 import Footer from "../componeten/Footer";
 import google from "../../../public/google.png";
+import {auth , provider} from '../../../src/firebase';
+import {signInWithPopup} from 'firebase/auth';
+import { FormEvent} from 'react';
+import { useRouter } from 'next/navigation';
+
+
 
 
 export default function inlog() {
+  //Routing 
+  const router = useRouter();
+  //Form event handler
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+  };
+  //Gooegle popup form 
+   const handleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log(result);
+      router.push('/gebruikers'); 
+    //  return result.user;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
   return (
     <>
     <Header />
     <div className="home_hero inlog_form">
-       <form>
+       <form onSubmit={handleSubmit}>
         <h2>Log in</h2>
 
         <input type="email"  placeholder="E-mail"/>
@@ -17,10 +42,10 @@ export default function inlog() {
 
         <input type="submit" value="Inloggen" />
         <div className="google">
-          <a href=""><Image src={google} alt="Google" /></a>
+          <Image src={google} onClick={handleLogin} alt="Google" />
           <a href="">Wachtwoord vergeten?</a>
         </div>
-        <a href="maakAccound">Geen account? Sign up</a>
+        <a href="maakaccound">Geen account? Sign up</a>
 
        </form>
     </div>
